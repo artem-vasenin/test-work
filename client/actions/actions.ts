952099bcf76cc523d181d0be965a4c8eea9973d7
artem_vasenin <rusich.accenture@mail.ru>
2017-10-05 10:16:56 +0300

    Добавил таймацт (чтобы отображалась "Зарузка")

--- client/actions/actions.ts
+++ client/actions/actions.ts
@@ -14,22 +14,18 @@ export function search(value: string) {
 
 export function getList(value: string) {
 	return dispatch => {
-		// dispatch({type: 'getList_BEGIN', value: value, isLoading: true});
-		// setTimeout(() => {
-		// 	console.log('tracks!!!');
-		// 	dispatch({type: 'getList_SUCCESS', value: value, isLoading: false});
-		// }, 2000);
-
 		dispatch({type: 'getList_BEGIN', value: value, isLoading: true});
 
 		var xhr = new XMLHttpRequest();
 		xhr.open('GET', 'http://cyties.test:8080/', false);
 		xhr.send();
 		if (xhr.status != 200) {
-			dispatch({type: 'getList_ERROR', value: 'нет доступа к серверу', isError: true});		  	
+			dispatch({type: 'getList_ERROR', value: 'нет доступа к серверу', isError: true});	
 		} else {
 			const remoteValue = JSON.parse(xhr.responseText);
-			dispatch({type: 'getList_SUCCESS', value: value, isLoading: false, isError: false, list: remoteValue});
+			setTimeout(() => {
+				dispatch({type: 'getList_SUCCESS', value: value, isLoading: false, isError: false, list: remoteValue});
+			}, 500);
 		}
 	}
 }
